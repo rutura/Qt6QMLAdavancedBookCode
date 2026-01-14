50ad25505cd2d4caacba79933fef0f392302ac1e
Daniel Gakwaya <wayadn88@gmail.com>
2026-01-14 07:38:37 +0300

    Initial stages of the contact list model

---
+++ 03_ContactManager/04_ContactListModel/main.cpp
@@ -0,0 +1,22 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    // Set Fusion style to support custom controls
+    QQuickStyle::setStyle("Fusion");
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("ContactManagerApp", "Main");
+
+    return app.exec();
+}
