629f13946bab45c754d6048dcd117651873104a0
Daniel Gakwaya <wayadn88@gmail.com>
2026-05-23 11:17:26 +0300

    refactor: Remove unused repository fetching methods and related properties from GitHubService

--- 04_RestPoweredModelsCachingThreading/02_rest_backed_model/src/api/githubservice.h
+++ 04_RestPoweredModelsCachingThreading/02_rest_backed_model/src/api/githubservice.h
@@ -18,7 +18,6 @@ class GitHubService : public QObject
     Q_PROPERTY(QString authToken READ authToken WRITE setAuthToken NOTIFY authTokenChanged)
     Q_PROPERTY(bool isLoading READ isLoading NOTIFY isLoadingChanged)
     Q_PROPERTY(QString errorMessage READ errorMessage NOTIFY errorMessageChanged)
-    Q_PROPERTY(QVariantList repositories READ repositories NOTIFY repositoriesChanged)
 
 public:
     explicit GitHubService(QObject *parent = nullptr);
@@ -26,43 +25,32 @@ public:
     QString authToken() const { return m_authToken; }
     bool isLoading() const { return m_isLoading; }
     QString errorMessage() const { return m_errorMessage; }
-    QVariantList repositories() const { return m_repositories; }
 
     void setAuthToken(const QString &token);
 
-    Q_INVOKABLE void fetchUserRepositories(const QString &username = QString());
-    Q_INVOKABLE void fetchRepository(const QString &owner, const QString &repo);
-    Q_INVOKABLE void clearRepositories();
     Q_INVOKABLE void searchRepositories(const QString &query, const QString &sort = "stars", const QString &order = "desc");
-    Q_INVOKABLE void fetchAuthenticatedUserRepositories();
 
 signals:
     void authTokenChanged();
     void isLoadingChanged();
     void errorMessageChanged();
-    void repositoriesChanged();
-    void repositoryFetched(const QVariant &repository);
 
     // NEW: Raw-typed signal consumed by the new RepositoryListModel.
     // The Repository* objects emitted have no parent. The receiver takes ownership.
     void searchResultsReady(const QList<Repository*> &repositories);
 
 private slots:
-    void onUserRepositoriesReceived();
-    void onRepositoryReceived();
     void onSearchResultsReceived();
     void onRequestFailed(QNetworkReply::NetworkError error);
 
 private:
     void setIsLoading(bool loading);
     void setErrorMessage(const QString &message);
-    QVariant parseRepositoryJson(const QJsonObject &json);
 
     QNetworkAccessManager *m_networkManager;
     QString m_authToken;
     bool m_isLoading = false;
     QString m_errorMessage;
-    QVariantList m_repositories;
 };
 
 #endif // GITHUBSERVICE_H
